f7c5e1e9deb0c468fed473673caff18b8f9fad5e
Chris Cedeno <chrisrey95@gmail.com>
2019-04-03 12:49:47 -0400

    added MUL case and reading mult file

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -1,31 +1,55 @@
 #include "cpu.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define DATA_LEN 6
+
+unsigned char cpu_ram_read(struct cpu *cpu, unsigned char index) 
+{
+  return cpu->ram[index];
+}
+
+void cpu_ram_write(struct cpu *cpu, int index, unsigned char val) 
+{
+  cpu->ram[index] = val;
+}
 
 /**
  * Load the binary bytes from a .ls8 source file into a RAM array
  */
 void cpu_load(struct cpu *cpu)
 {
-  char data[DATA_LEN] = {
-    // From print8.ls8
-    0b10000010, // LDI R0,8
-    0b00000000,
-    0b00001000,
-    0b01000111, // PRN R0
-    0b00000000,
-    0b00000001  // HLT
-  };
+  // char data[DATA_LEN] = {
+  //   // From print8.ls8
+  //   0b10000010, // LDI R0,8
+  //   0b00000000,
+  //   0b00001000,
+  //   0b01000111, // PRN R0
+  //   0b00000000,
+  //   0b00000001  // HLT
+  // };
 
-  int address = 0;
+  // int address = 0;
 
-  for (int i = 0; i < DATA_LEN; i++) {
-    cpu->ram[address++] = data[i];
-  }
+  // for (int i = 0; i < DATA_LEN; i++) {
+  //   cpu->ram[address++] = data[i];
+  // }
 
   // TODO: Replace this with something less hard-coded
+
+  FILE *fp;
+  char line[1024];
+
+  fp = fopen("./examples/mult.ls8", "r");
+
+  int count = 0;
+  while (fgets(line, 1024, fp) != NULL) {
+    char *ptr;
+    unsigned char ret = strtoul(line, &ptr, 2);
+    // printf("%u\n", ret);
+    cpu_ram_write(cpu, count++, ret);
+  }
 }
 
 /**
@@ -40,16 +64,6 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
 
     // TODO: implement more ALU ops
   }
-}
-
-unsigned char cpu_ram_read(struct cpu *cpu, unsigned char index) 
-{
-  return cpu->ram[index];
-}
-
-void cpu_ram_write(struct cpu *cpu, unsigned char index, unsigned char val) 
-{
-  cpu->ram[index] = val;
 }
 
 /**
@@ -89,6 +103,13 @@ void cpu_run(struct cpu *cpu)
 
       case LDI:
         cpu->registers[operandA] = operandB;
+        cpu->pc = cpu->pc + 2;
+        break;
+
+      case MUL:
+        // printf("A: %d\n", cpu->registers[operandA]);
+        // printf("B: %d\n", cpu->registers[operandB]);
+        cpu->registers[operandA] = cpu->registers[operandA] * cpu->registers[operandB];
         cpu->pc = cpu->pc + 2;
         break;
       
