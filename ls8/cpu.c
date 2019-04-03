2f96d40bc62e21f3a46a25c443d44747a4b18e9b
Chris Cedeno <chrisrey95@gmail.com>
2019-04-03 13:18:33 -0400

    utilized alu function

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -37,8 +37,8 @@ void cpu_load(struct cpu *cpu, int argc, char *argv[] )
       cpu->ram[address++] = data[i];
     }
   } else {
-    // TODO: Replace this with something less hard-coded
 
+    // TODO: Replace this with something less hard-coded
     FILE *fp;
     char line[1024];
 
@@ -48,7 +48,6 @@ void cpu_load(struct cpu *cpu, int argc, char *argv[] )
     while (fgets(line, 1024, fp) != NULL) {
       char *ptr;
       unsigned char ret = strtoul(line, &ptr, 2);
-      // printf("%u\n", ret);
       cpu_ram_write(cpu, count++, ret);
     }
   }
@@ -62,8 +61,9 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
   switch (op) {
     case ALU_MUL:
       // TODO
+      cpu->registers[regA] = cpu->registers[regA] * cpu->registers[regB];
+      cpu->pc = cpu->pc + 2;
       break;
-
     // TODO: implement more ALU ops
   }
 }
@@ -75,10 +75,6 @@ void cpu_run(struct cpu *cpu)
 {
   int running = 1; // True until we get a HLT instruction
 
-  // #define LDI  0b10000010
-  // #define HLT  0b00000001
-  // #define PRN  0b01000111
-
   while (running) {
     // TODO
     // 1. Get the value of the current instruction (in address PC).
@@ -88,13 +84,6 @@ void cpu_run(struct cpu *cpu)
     // 3. Get the appropriate value(s) of the operands following this instruction
     unsigned char operandA = cpu_ram_read(cpu, cpu->pc + 1);
     unsigned char operandB = cpu_ram_read(cpu, cpu->pc + 2);
-
-    // 0b10000010, // LDI R0,8
-    // 0b00000000,
-    // 0b00001000,
-    // 0b01000111, // PRN R0
-    // 0b00000000,
-    // 0b00000001  // HLT
 
     // 4. switch() over it to decide on a course of action.
     // 5. Do whatever the instruction should do according to the spec.
@@ -109,10 +98,7 @@ void cpu_run(struct cpu *cpu)
         break;
 
       case MUL:
-        // printf("A: %d\n", cpu->registers[operandA]);
-        // printf("B: %d\n", cpu->registers[operandB]);
-        cpu->registers[operandA] = cpu->registers[operandA] * cpu->registers[operandB];
-        cpu->pc = cpu->pc + 2;
+        alu(cpu, ALU_MUL, operandA, operandB);
         break;
       
       case PRN:
@@ -123,7 +109,6 @@ void cpu_run(struct cpu *cpu)
       default:
         break;
     }
-
     // 6. Move the PC to the next instruction.
     cpu->pc++;
   }
